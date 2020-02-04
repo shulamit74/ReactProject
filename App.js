3ca7776880cae1821d0af8715018a35ef764a0a3
adiAlicecode <58771667+adiAlicecode@users.noreply.github.com>
2020-02-04 15:04:30 +0200

    need to change from console to page

---
+++ App.js
@@ -0,0 +1,58 @@
+import React, { Fragment ,Component} from 'react';
+
+const list=[1,2,3,4,5,6];
+
+class App extends Component
+{
+  state={
+    index: 0
+  };
+
+  //List=list.map((item)=>{
+    //if(item === 1)
+      //return <li className='active'>{item}</li>;
+  //});
+
+  changeListForward=()=>{
+    let i = (this.state.index)
+
+    if(i>=0 && i < list.length)
+      this.setState({
+        index: i + 1
+      },()=>{
+        console.log(list[this.state.index])
+      })
+    }
+  
+  changeListback=()=>{
+    let i = (this.state.index)
+
+    if(i>0)
+      this.setState({
+        index: i - 1
+      },()=>{
+        console.log(list[this.state.index])
+      })
+   }
+
+  render(){
+    return(
+      <Fragment>
+        <h1>The list:</h1>
+        <ul>
+          {list.map(item=>(
+          <li>{item}</li>
+          ))}
+        </ul>
+
+        <div>
+          <h1>Click the buttons to see the rest of the list</h1>
+          <button id="forward" onClick={this.changeListForward}>forward</button>
+          <button id="back" onClick={this.changeListback}>back</button>
+        </div> 
+      </Fragment>
+    );
+  }
+}
+
+export default App;
